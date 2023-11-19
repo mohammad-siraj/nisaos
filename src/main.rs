48dd1ad0ea70a31e88e243427b80717d19a37a72
Mohammad Siraj <mohammadsiraj@Mohammads-MacBook-Air.local>
2023-11-19 23:36:45 +0530

    creating rust binary for os kernal functioning

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+#![no_main]//disable the main rust level entry points
+#![no_std]// disable auto linking of std libraries
+
 use core::panic::PanicInfo;
+
+#[no_mangle] // for not mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+    loop {}
+}
+
 
 /// This function is called on panic. To handle panic
 #[panic_handler]
